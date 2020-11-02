fbedf9f5eca71de5cb952db068f4db998390ec8d
leebernard <ldbernar@ucsc.edu>
2020-11-02 15:42:27 -0700

    Finished up hw 6

--- hw6.py
+++ hw6.py
@@ -15,7 +15,7 @@ def solar_fun(d_new, baseline_constant=1365):
     return baseline_constant/(d_new**2)
 
 
-def equil_temp(d_new, A):
+def equil_temp(d_new, A, baseline_constant=1365):
     # equation for equilibrium temperature
     '''
     T_new = 1/sqrt(d) * T_earth works, but does not account for albedo
@@ -26,14 +26,14 @@ def equil_temp(d_new, A):
     # T_new = (s_new * (1 - A)/4*sigma)**(1/4)
     # sigma = stefan-boltzmann constant = 5.67e10−8 W⋅m−2⋅K−4
     sigma = 5.67e-8
-    return (solar_fun(d_new) * (1 - A)/(4*sigma))**(1/4)
+    return (solar_fun(d_new, baseline_constant=baseline_constant) * (1 - A)/(4*sigma))**(1/4)
 
 
 def blackbody(wavelength, temp=300):
     # expects wavelength in meters
 
     c = 3e8  # um/s
-    h = 6.626e-34  # Js
+    h = 6.626e-34  # J
     k = 1.38e-23  # J/K
 
     # returns W m^-2 m^-1 sr^-1
@@ -62,6 +62,8 @@ def find_intersection(set1, set2):
 c = 3e8  # m/s
 h = 6.626e-34  # Js
 k = 1.38e-23  # J/K
+
+
 '''
 Problem 1
 '''
@@ -71,25 +73,28 @@ with open('hw1_data/data.yaml') as file:
     planet_data = yaml.load(file, Loader=yaml.FullLoader)
 
 
-def prob1_plot_data(raw_wavelengths, distance, albedo, name):
+def prob1_plot_data(raw_wavelengths, distance, albedo, name,
+                    r_star=.00465, star_temp=5800, baseline_constant=1365):
     # expects wavelengths in meters, outputs in um
-    earth_temp = equil_temp(distance, albedo)
+    planet_temp = equil_temp(distance, albedo, baseline_constant=baseline_constant)
+    print('Planet temp:', planet_temp)
 
-    earth_spectrum = blackbody(raw_wavelengths, temp=earth_temp)
-    reflected_spectrum = reflected_rad(raw_wavelengths, distance, albedo)
+    planet_spectrum = blackbody(raw_wavelengths, temp=planet_temp)
+    reflected_spectrum = reflected_rad(raw_wavelengths, distance, albedo,
+                                       r_star=r_star, temp=star_temp)
 
     # convert from per meter to per micrometer
-    earth_spectrum = earth_spectrum * 1e-6
+    planet_spectrum = planet_spectrum * 1e-6
     reflected_spectrum = reflected_spectrum * 1e-6
     wavelengths = raw_wavelengths * 1e6
 
-    idx = find_intersection(earth_spectrum, reflected_spectrum)
+    idx = find_intersection(planet_spectrum, reflected_spectrum)
     intersection_wavelength = wavelengths[idx]
 
     plt.figure(name, figsize=(8, 6))
-    plt.plot(wavelengths, earth_spectrum)
+    plt.plot(wavelengths, planet_spectrum)
     plt.plot(wavelengths, reflected_spectrum)
-    plt.plot(wavelengths[idx], earth_spectrum[idx], 'ro')
+    plt.plot(wavelengths[idx], planet_spectrum[idx], 'ro')
     plt.legend((name + ' emission',
                 'Reflected light',
                 'Intersection at %.2f μm' %intersection_wavelength))
@@ -120,6 +125,55 @@ intersection_list.append(prob1_plot_data(np.linspace(2e-6, 100e-6, num=500),
                                          curr_data['albedo'],
                                          'Planet9'))
 
+# for exoplanet, rescale the solar constant
+new_insolation = 1365 * (1.15)**2 * (6065/5800)**4
+intersection_list.append(prob1_plot_data(raw_wavelengths=np.linspace(.5e-6, 5e-6, num=500),
+                         distance=.047,
+                         albedo=.01,
+                         name='Hot Jup',
+                         r_star=.00465*1.15, star_temp=6065, baseline_constant=new_insolation))
+
+
+
+'''
+Problem 4
+'''
+
+def rayleigh_jeans(wavelength, temp=300):
+    # expects wavelength in meters
+
+    c = 3e8  # um/s
+    h = 6.626e-34  # Js
+    k = 1.38e-23  # J/K
+
+    # returns W m^-2 m^-1 sr^-1
+    return 2*c*k/(wavelength**4) * temp
+
+
+raw_wavelengths = np.linspace(1e-6, 2500e-6, num=1000)
+wavelengths = raw_wavelengths * 1e6
+
+bb_emission = blackbody(raw_wavelengths, temp=300) * 1e-6  # in um
+approx_emission = rayleigh_jeans(raw_wavelengths, temp=300) * 1e-6
+
+percent_difference = (approx_emission - bb_emission)/bb_emission * 100
+percent_difference = np.around(percent_difference, decimals=3)
+idx = np.nonzero(percent_difference == 1.0)[0][0]
+one_percent_wavelength = wavelengths[idx]
+
+plt.figure('RJ approx', figsize=(8, 6))
+plt.plot(wavelengths, bb_emission)
+plt.plot(wavelengths, approx_emission)
+plt.plot(wavelengths[idx], approx_emission[idx], 'ro')
+
+plt.legend(('Black body emission',
+            'Rayleigh Jeans approx',
+            '1%% difference at %d μm' %int(one_percent_wavelength)))
+plt.xlabel('Wavelength (μm)')
+plt.ylabel('W m^-2 μm^-1 sr^-1')
+plt.yscale('log')
+plt.title('Blackbody at 300K')
+
 
 
 
@@ -131,3 +185,51 @@ wave = 10
 power = blackbody(wave, temp=300)
 power * wave / (h*c)
 # 4.97e26
+
+
+'''
+Problem 6
+'''
+
+def freq_blackbody(freq, temp=300):
+    # expects wavelength in meters
+
+    c = 3e8  # um/s
+    h = 6.626e-34  # Js
+    k = 1.38e-23  # J/K
+
+    # returns W m^-2 m^-1 sr^-1
+    return (2*h*freq**3 / c**2) * 1/(np.exp(h*freq / (temp*k)) - 1)
+
+
+raw_wavelengths = np.linspace(1e-6, 20e-6, num=1000)
+
+wave_spectrum = blackbody(raw_wavelengths, temp=700)
+
+plt.figure('test', figsize=(8, 6))
+plt.plot(raw_wavelengths*1e6, wave_spectrum*1e-6)
+
+plt.xlabel('Wavelength (μm)')
+plt.ylabel('W m^-2 μm^-1 sr^-1')
+
+# convert to freq
+freq_spectrum = wave_spectrum * (raw_wavelengths**2)/c
+
+freqs = c/raw_wavelengths
+
+theo_freq_spectrum = freq_blackbody(freqs, temp=700)
+
+tera_freqs = freqs * 1e-12  # gigahertz
+plt.figure('Freqencies', figsize=(8, 6))
+plt.plot(tera_freqs, freq_spectrum*1e12, 'o')
+plt.plot(tera_freqs, theo_freq_spectrum*1e12)
+plt.xlabel('Frequency (THz)')
+plt.ylabel('W m^-2 GHz^-1 sr^-1')
+plt.legend(('Converted',
+            'Actual'))
+plt.title('Frequency converted from wavelength for a 700K blackbody')
+
+
+
+
+
